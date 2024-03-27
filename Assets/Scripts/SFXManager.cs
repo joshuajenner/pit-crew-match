a6e87cf69e309aeacfde4d6f7ccdb2727118fdc5
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2024-03-28 01:48:07 +0800

    Added SFX Manager

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioClip swipe;
+    public AudioClip swapBack;
+    public AudioClip match;
+
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
